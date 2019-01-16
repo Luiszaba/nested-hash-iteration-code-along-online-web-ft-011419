8fd786fb3d4b23c8f20ae8d42f6573241a53f500
Luis Zabala <luis.zabala076@gmail.com>
2019-01-16 17:57:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |name, data|
+  data.each do |attribute, details|
+    if attribute == :favorite_ice_cream_flavors {|ice_cream| ice_cream == "strawberry"}
+      
 end
